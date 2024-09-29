9907a0214e69d6b6fa1a9ec4e9ffafe03f0478f4
Michael <michaelorihuela00@gmail.com>
2024-09-29 07:53:24 -0500

    Corrección Dockerfile #4

--- appGestionBanco/obj/Debug/net8.0/appGestionBanco.AssemblyInfo.cs
+++ appGestionBanco/obj/Debug/net8.0/appGestionBanco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appGestionBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9eb03725db62f482e41e834293527a69ed0121e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ca9b43ab2f3ae1a9897b16cfa99a1162aa15bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("appGestionBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appGestionBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
