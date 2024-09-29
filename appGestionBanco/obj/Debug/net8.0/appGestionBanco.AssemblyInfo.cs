1231433b9a7601bf2d5d58afd036d164578b1fa6
Michael <michaelorihuela00@gmail.com>
2024-09-29 07:45:33 -0500

    Corrección #3

--- appGestionBanco/obj/Debug/net8.0/appGestionBanco.AssemblyInfo.cs
+++ appGestionBanco/obj/Debug/net8.0/appGestionBanco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appGestionBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cc13988d98ccabe90f4d10214ad0e45d586f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9eb03725db62f482e41e834293527a69ed0121e")]
 [assembly: System.Reflection.AssemblyProductAttribute("appGestionBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appGestionBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
